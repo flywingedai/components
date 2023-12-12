b94d0f71ec2a44108be9010714e2a034708c157d
Clayton Brown <clay@flywinged.ai>
2023-12-12 11:15:30 -0700

    added ErrorIO tool

--- tests/utils.go
+++ tests/utils.go
@@ -3,8 +3,22 @@ package tests
 import "errors"
 
 // Helper to handle bad read errors
-type ErrorReader int
+type ErrorIO struct {
+	ReadErr  error
+	CloseErr error
+}
 
-func (ErrorReader) Read(p []byte) (n int, err error) {
-	return 0, errors.New("ErrorReader error")
+func NewDefaultErrorIO() *ErrorIO {
+	return &ErrorIO{
+		ReadErr:  errors.New("ErrorIO: Read error"),
+		CloseErr: errors.New("ErrorIO: Close error"),
+	}
+}
+
+func (e *ErrorIO) Read(p []byte) (n int, err error) {
+	return 0, e.ReadErr
+}
+
+func (e *ErrorIO) Close() error {
+	return e.CloseErr
 }

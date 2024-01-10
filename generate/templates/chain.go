df1924ed1fc665c54cfdd6618a78c11ae4e82077
Derek Tolbert <derek.s.tolbert@gmail.com>
2024-01-10 09:52:29 -0700

    change component ref from c to _C (#10)
    Co-authored-by: derek.t <derek@risingcloud.com>

--- generate/templates/chain.go
+++ generate/templates/chain.go
@@ -14,44 +14,44 @@ func Create_{{InterfaceName}}_ExpecterChain[M any{{GenericLongAppend}}](fetch fu
 	Chain = `
 type {{InterfaceName}}_{{Method}}Chain[M any{{GenericLongAppend}}] func(*M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}}
 
-func (c {{InterfaceName}}_ExpecterChain[M{{GenericShortAppend}}]) {{Method}}({{ArgsInterface}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_ExpecterChain[M{{GenericShortAppend}}]) {{Method}}({{ArgsInterface}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		expecter := c(m)
+		expecter := _c(m)
 		return expecter.{{Method}}({{ArgsShort}})
 	}
 }
 
-func (c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Run(run func({{Args}})) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Run(run func({{Args}})) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		call := c(m)
+		call := _c(m)
 		return call.Run(run)
 	}
 }
 
-func (c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Return({{ReturnsArgs}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Return({{ReturnsArgs}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		call := c(m)
+		call := _c(m)
 		return call.Return({{ReturnsShort}})
 	}
 }
 
-func (c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) RunAndReturn(run func({{Args}}){{ReturnsTypes}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) RunAndReturn(run func({{Args}}){{ReturnsTypes}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		call := c(m)
+		call := _c(m)
 		return call.RunAndReturn(run)
 	}
 }
 
-func (c {{InterfaceName}}_ExpecterChain[M{{GenericShortAppend}}]) {{Method}}_Pointer({{ArgsInterface}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_ExpecterChain[M{{GenericShortAppend}}]) {{Method}}_Pointer({{ArgsInterface}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		expecter := c(m)
+		expecter := _c(m)
 		return expecter.{{Method}}({{ArgsShortPointer}})
 	}
 }
 
-func (c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Return_Pointer({{ReturnsArgsPointer}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
+func (_c {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}]) Return_Pointer({{ReturnsArgsPointer}}) {{InterfaceName}}_{{Method}}Chain[M{{GenericShortAppend}}] {
 	return func(m *M) *{{InterfaceName}}_{{Method}}_Call{{GenericShort}} {
-		call := c(m)
+		call := _c(m)
 		return call.Return({{ReturnsShortPointer}})
 	}
 }
